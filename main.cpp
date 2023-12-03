4be97e5c8349da60e901ff52acbca4cc5bfe5d38
andreyv2048 <andreyv2048@gmail.com>
2023-12-03 12:33:34 +0500

    8

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #pragma once
 #include <iostream>
+using namespace std;
 int main(int argc, char** argv)
 {
+	cout << "hi" << endl;
+	list<int> list;
+	for(auto &l: list)
+	{
+		
+	}
 	return 0;
 }
